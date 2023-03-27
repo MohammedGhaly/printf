0366c5030a30739b7e7f77bce425369b7eff5649
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-27 12:59:24 -0700

    let's try

---
+++ helperFunctions2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * writedecimal - prints decimal numbers
+  * @n: input num
+  *
+  * Return: num length
+  */
+
+int writedecimal(int n)
+{
+	int i;
+	char *s;
+
+	for (i = 0; i < n; i++)
+		s[i] = n;
+	write(1, s, 1);
+	return (i);
+}
